d0ae36c7fe3ced52c5b0282e5dfa81851436a7ce
kradic <klararadic04@gmail.com>
2026-01-18 16:31:30 +0100

    Update JS form to validate email

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,12 +1,23 @@
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
+
 function submitForm()
 {
-    let fname = document.getElementById("fname");
-    let review = document.getElementById("review");
+    let fname = document.getElementById("fname")
+    let review = document.getElementById("review")
     let email = document.getElementById("email")
 
     if(fname.value == "" || review.value == "" || email.value == "")
     {
         alert("Please fill out all mandatory (*) fields!")
+    }
+    else if(!validateEmail(email.value)){
+        alert("Email should be written in the correct format, e.g. email@domain.com")
     }
     else
     {
